dfbaf0aaff0e0cdc7bc3884d254e3e926213bbc2
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2018-08-27 13:02:43 +0200

    arm64: cpufeature: Add feature for CRC32 instructions.
     * Add a CRC32 feature bit and wire it up to the CPU id register so we
       will be able to use alternatives patching for CRC32 operations.
    
    Change-Id: Ie0cf3a5537d3a3d2e44c0197c5eb4dcfd61642ea

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -60,7 +60,9 @@
 #define ARM64_WORKAROUND_1742098		39
 #define ARM64_HAS_SB				40
 #define ARM64_WORKAROUND_SPECULATIVE_SSBS	41
+#define ARM64_HAS_CRC32                         42
 
-#define ARM64_NCAPS				64
+/* kabi: reserve 42 - 65 for future cpu capabilities */
+#define ARM64_NCAPS				65
 
 #endif /* __ASM_CPUCAPS_H */
