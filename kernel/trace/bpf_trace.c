002336aef2167aa37e8e52e517507e47a9a5ca67
Pavel Dubrova <pashadubrova@gmail.com>
2019-11-29 13:05:51 +0200

    trace: Add Kconfig option for trace_printk()
    Trace_printk() was used to log debug messages where printk()
    isn't feasible. As there should not be a single caller to
    trace_printk() in production kernels, replace them with empty
    functions.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -507,6 +507,7 @@ fmt_next:
 /* Horrid workaround for getting va_list handling working with different
  * argument type combinations generically for 32 and 64 bit archs.
  */
+#ifdef CONFIG_TRACE_PRINTK
 #define __BPF_TP_EMIT()	__BPF_ARG3_TP()
 #define __BPF_TP(...)							\
 	bpf_do_trace_printk(fmt, ##__VA_ARGS__)
@@ -533,6 +534,9 @@ fmt_next:
 	      : __BPF_ARG2_TP((u32)arg3, ##__VA_ARGS__)))
 
 	return __BPF_TP_EMIT();
+#else
+	return 0;
+#endif /* CONFIG_TRACE_PRINTK */
 }
 
 static const struct bpf_func_proto bpf_trace_printk_proto = {
