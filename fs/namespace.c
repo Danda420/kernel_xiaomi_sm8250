73d9cfe321eeb4a3cab98f5a5d43ffef7f8f6f71
Christoph Hellwig <hch@lst.de>
2020-07-21 10:54:34 +0200

    fs: refactor ksys_umount
    Factor out a path_umount helper that takes a struct path * instead of the
    actual file name.  This will allow to convert the init and devtmpfs code
    to properly mount based on a kernel pointer instead of relying on the
    implicit set_fs(KERNEL_DS) during early init.
    
    * Inline with KernelSU
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1679,53 +1679,18 @@ int path_umount(struct path *path, int flags)
 	return ret;
 }
 
-/*
- * Now umount can handle mount points as well as block devices.
- * This is important for filesystems which use unnamed block devices.
- *
- * We now support a flag for forced unmount like the other 'big iron'
- * unixes. Our API is identical to OSF/1 to avoid making a mess of AMD
- */
-
 int ksys_umount(char __user *name, int flags)
 {
-	struct path path;
-	struct mount *mnt;
-	int retval;
 	int lookup_flags = 0;
-
-	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
-		return -EINVAL;
-
-	if (!may_mount())
-		return -EPERM;
+	struct path path;
+	int ret;
 
 	if (!(flags & UMOUNT_NOFOLLOW))
 		lookup_flags |= LOOKUP_FOLLOW;
-
-	retval = user_path_mountpoint_at(AT_FDCWD, name, lookup_flags, &path);
-	if (retval)
-		goto out;
-	mnt = real_mount(path.mnt);
-	retval = -EINVAL;
-	if (path.dentry != path.mnt->mnt_root)
-		goto dput_and_out;
-	if (!check_mnt(mnt))
-		goto dput_and_out;
-	if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
-		goto dput_and_out;
-	retval = -EPERM;
-	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
-		goto dput_and_out;
-
-	retval = do_umount(mnt, flags);
-dput_and_out:
-	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
-	dput(path.dentry);
-	mntput_no_expire(mnt);
-
-out:
-	return retval;
+	ret = user_path_mountpoint_at(AT_FDCWD, name, lookup_flags, &path);
+	if (ret)
+		return ret;
+	return path_umount(&path, flags);
 }
 
 SYSCALL_DEFINE2(umount, char __user *, name, int, flags)
