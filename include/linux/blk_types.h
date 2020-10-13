d4d1547777010f90a2d429289250e762b0847ae0
a17671 <a17671@motorola.com>
2020-10-13 18:07:47 +0800

    ufs:Add Samsung UFS3.1 drivers headers
    Header file changes for Samsung
    HPB_FS and the UFSFEATURE_31
    
    Change-Id: I972820a59f5c70916f0f636796a8703c15ab482d
    Signed-off-by: a17671 <a17671@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1773953
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Wei Deng <dengwei1@motorola.com>
    Reviewed-by: Qing Chang <qing@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/blk_types.h
+++ include/linux/blk_types.h
@@ -349,6 +349,7 @@ enum req_flag_bits {
 	/* for wbt use*/
 	__REQ_WBT,
 
+	__REQ_HPB_PREFER,	/* HPB Flag */
 	__REQ_NR_BITS,		/* stops here */
 };
 
@@ -372,9 +373,9 @@ enum req_flag_bits {
 
 #define REQ_DRV			(1ULL << __REQ_DRV)
 #define REQ_SWAP		(1ULL << __REQ_SWAP)
-
 #define REQ_WBT 		(1ULL << __REQ_WBT)
 
+#define REQ_HPB_PREFER          (1ULL << __REQ_HPB_PREFER)
 #define REQ_FAILFAST_MASK \
 	(REQ_FAILFAST_DEV | REQ_FAILFAST_TRANSPORT | REQ_FAILFAST_DRIVER)
 
