296509526c51b9003ce697e577169709ad3e154f
Xuewen Yan <xuewen.yan@unisoc.com>
2021-03-25 10:49:18 +0800

    FROMLIST: BACKPORT: Fix task utilization accountability in compute_energy()
    [root cause  ] eas optimization
    [changes     ] porting maillist
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:sharkl5pro
    [test case]:no
    [reviewers   ] ke.wang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    find_energy_efficient_cpu() (feec()) computes for each perf_domain (pd) an
    energy delta as follows:
    
      feec(task)
        for_each_pd
          base_energy = compute_energy(task, -1, pd)
            -> for_each_cpu(pd)
               -> cpu_util_next(cpu, task, -1)
    
          energy_delta = compute_energy(task, dst_cpu, pd)
            -> for_each_cpu(pd)
               -> cpu_util_next(cpu, task, dst_cpu)
          energy_delta -= base_energy
    
    Then it picks the best CPU as being the one that minimizes energy_delta.
    
    cpu_util_next() estimates the CPU utilization that would happen if the
    task was placed on dst_cpu as follows:
    
      max(cpu_util + task_util, cpu_util_est + _task_util_est)
    
    The task contribution to the energy delta can then be either:
    
      (1) _task_util_est, on a mostly idle CPU, where cpu_util is close to 0
          and _task_util_est > cpu_util.
      (2) task_util, on a mostly busy CPU, where cpu_util > _task_util_est.
    
      (cpu_util_est doesn't appear here. It is 0 when a CPU is idle and
       otherwise must be small enough so that feec() takes the CPU as a
       potential target for the task placement)
    
    This is problematic for feec(), as cpu_util_next() might give an unfair
    advantage to a CPU which is mostly busy (2) compared to one which is
    mostly idle (1). _task_util_est being always bigger than task_util in
    feec() (as the task is waking up), the task contribution to the energy
    might look smaller on certain CPUs (2) and this breaks the energy
    comparison.
    
    This issue is, moreover, not sporadic. By starving idle CPUs, it keeps
    their cpu_util < _task_util_est (1) while others will maintain cpu_util >
    _task_util_est (2).
    
    Fix this problem by always using max(task_util, _task_util_est) as a task
    contribution to the energy (ENERGY_UTIL). The new estimated CPU
    utilization for the energy would then be:
    
      max(cpu_util, cpu_util_est) + max(task_util, _task_util_est)
    
    compute_energy() still needs to know which OPP would be selected if the
    task would be migrated in the perf_domain (FREQUENCY_UTIL). Hence,
    cpu_util_next() is still used to estimate the maximum util within the pd.
    
    The sub_positive local version is saving an explicit load-store and is
    enough for the cpu_util_next() usage.
    
    https://lore.kernel.org/lkml/20210225083612.1113823-3-vincent.donnefort@arm.com/
    https://lore.kernel.org/lkml/20210225083612.1113823-2-vincent.donnefort@arm.com/
    
    [diphons]
    Adapted for 4.19
    
    Change-Id: I1d1434fa3f50039d32c3bf6288f729f277c6da14
    Signed-off-by: Vincent Donnefort <vincent.donnefort@arm.com>
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8079,7 +8079,7 @@ static unsigned long cpu_util_next(int cpu, struct task_struct *p, int dst_cpu)
 	 * util_avg should already be correct.
 	 */
 	if (task_cpu(p) == cpu && dst_cpu != cpu)
-		sub_positive(&util, task_util(p));
+		lsub_positive(&util, task_util(p));
 	else if (task_cpu(p) != cpu && dst_cpu == cpu)
 		util += task_util(p);
 
@@ -8185,9 +8185,21 @@ compute_energy(struct task_struct *p, int dst_cpu, struct perf_domain *pd)
 			cpu_util = cpu_util_next_walt(cpu, p, dst_cpu);
 			sum_util += cpu_util;
 #else
-			unsigned int util_cfs;
+			unsigned long util_freq = cpu_util_next(cpu, p, dst_cpu);
+			unsigned long util_running = util_freq;
 
-			util_cfs = cpu_util_next(cpu, p, dst_cpu);
+			/*
+			 * When @p is placed on @cpu:
+			 *
+			 * util_running = max(cpu_util, cpu_util_est) +
+			 *		  max(task_util, _task_util_est)
+			 *
+			 * while cpu_util_next is: max(cpu_util + task_util,
+			 *			       cpu_util_est + _task_util_est)
+			 */
+			if (cpu == dst_cpu) {
+				util_running = cpu_util_next(cpu, p, -1) + task_util_est(p);
+			}
 
 			/*
 			 * Busy time computation: utilization clamping is not
@@ -8195,7 +8207,7 @@ compute_energy(struct task_struct *p, int dst_cpu, struct perf_domain *pd)
 			 * is already enough to scale the EM reported power
 			 * consumption at the (eventually clamped) cpu_capacity.
 			 */
-			sum_util += schedutil_cpu_util(cpu, util_cfs, NULL, NULL);
+			sum_util += schedutil_cpu_util(cpu, util_running, NULL, NULL);
 
 			/*
 			 * Performance domain frequency: utilization clamping
@@ -8204,7 +8216,7 @@ compute_energy(struct task_struct *p, int dst_cpu, struct perf_domain *pd)
 			 * NOTE: in case RT tasks are running, by default the
 			 * FREQUENCY_UTIL's utilization can be max OPP.
 			 */
-			cpu_util = schedutil_cpu_util(cpu, util_cfs, &min, &max);
+			cpu_util = schedutil_cpu_util(cpu, util_freq, &min, &max);
 			/* Task's uclamp can modify min and max value */
 			if (uclamp_is_used()) {
 				min = max(min, uclamp_eff_value(p, UCLAMP_MIN));
