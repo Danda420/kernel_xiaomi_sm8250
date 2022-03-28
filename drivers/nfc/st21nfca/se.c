f929763ac42edb20873cb630ec517bbe61f1e5b3
goodmeow <harunbam3@gmail.com>
2022-03-28 21:16:50 +0530

    Merge branch 'android-4.19-stable' of https://android.googlesource.com/kernel/common into arrow-12.1
     Conflicts:
    	drivers/nfc/st21nfca/se.c

--- drivers/nfc/st21nfca/se.c
+++ drivers/nfc/st21nfca/se.c
@@ -351,6 +351,12 @@ int st21nfca_connectivity_event_received(struct nfc_hci_dev *hdev, u8 host,
 			return -EPROTO;
 		}
 
+		transaction->params_len = skb->data[transaction->aid_len + 3];
+
+		/* Total size is allocated (skb->len - 2) minus fixed array members */
+		if (transaction->params_len > ((skb->len - 2) - sizeof(struct nfc_evt_transaction)))
+			return -EINVAL;
+
 		memcpy(transaction->params, skb->data +
 		       transaction->aid_len + 4, transaction->params_len);
 
