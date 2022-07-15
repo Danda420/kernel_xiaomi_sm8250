54778f761819f9c7b708b99ae3c8075143895bc5
John Galt <johngaltfirstrun@gmail.com>
2022-07-15 10:00:41 -0400

    soc: qcom: ssr: Prevent kernel panic caused by adsp crash
    Let's just catch this and attempt to restart adsp.
    Because users do weird things.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -1258,7 +1258,7 @@ int subsystem_restart_dev(struct subsys_device *dev)
 		return 0;
 	}
 
-	if (!strcmp(name, "modem"))
+	if (!strcmp(name, "modem") || !strcmp(name, "adsp"))
 		dev->restart_level = RESET_SUBSYS_COUPLED;
 
         __subsystem_restart_dev(dev);
