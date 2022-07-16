80eaaccb5442e1d3084d09f991613f5fb939b1cf
EmanuelCN <emanuelghub@gmail.com>
2022-07-16 10:35:40 +0300

    schedutil: Hardcode rate limits for tri-cluster configuration
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Co-authored-by: Adam W. Willis <return.of.octobot@gmail.com>
    Co-authored-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -16,6 +16,7 @@
 #include <linux/sched/cpufreq.h>
 #include <trace/events/power.h>
 #include <linux/sched/sysctl.h>
+#include <linux/binfmts.h>
 
 struct sugov_tunables {
 	struct gov_attr_set	attr_set;
@@ -932,6 +933,9 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -951,6 +955,9 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
+
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
@@ -1289,6 +1296,21 @@ static int sugov_init(struct cpufreq_policy *policy)
 		tunables->rtg_boost_freq = DEFAULT_CPU7_RTG_BOOST_FREQ;
 		break;
 	}
+
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
+		tunables->up_rate_limit_us = 5000;
+		tunables->down_rate_limit_us = 5000;
+	}
+
+	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
+		tunables->up_rate_limit_us = 16000;
+		tunables->down_rate_limit_us = 4000;
+	}
+
+        if (cpumask_test_cpu(policy->cpu, cpu_prime_mask)) {
+                tunables->up_rate_limit_us = 16000;
+                tunables->down_rate_limit_us = 4000;
+        }
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
