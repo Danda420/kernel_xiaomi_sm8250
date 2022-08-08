083d84536e767496c6588cd648e2000242925edf
Kazuki Hashimoto <kazukih@tuta.io>
2022-08-09 02:39:54 +0900

    mm: watermark_scale_factor: Set to 32
    Mimicking OnePlus.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -337,7 +337,7 @@ int watermark_boost_factor __read_mostly;
 #else
 int watermark_boost_factor __read_mostly = 0;
 #endif
-int watermark_scale_factor = 10;
+int watermark_scale_factor = 32;
 
 /*
  * Extra memory for the system to try freeing. Used to temporarily
