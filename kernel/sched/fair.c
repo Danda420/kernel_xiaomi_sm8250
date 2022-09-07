fd0cee563fe9cd86e1268413391bc1a24626da27
Abel Wu <wuyun.abel@bytedance.com>
2022-09-07 19:19:57 +0800

    sched/fair: Avoid double search on same cpu
    The prev cpu is checked at the beginning of SIS, and it's unlikely
    to be idle before the second check in select_idle_smt(). So we'd
    better focus on its SMT siblings.
    
    Signed-off-by: Abel Wu <wuyun.abel@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Josh Don <joshdon@google.com>
    Acked-by: Mel Gorman <mgorman@techsingularity.net>
    Link: https://lore.kernel.org/r/20220907112000.1854-3-wuyun.abel@bytedance.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7077,7 +7077,7 @@ static int select_idle_smt(struct task_struct *p, struct sched_domain *sd, int t
 		return -1;
 
 	for_each_cpu(cpu, cpu_smt_mask(target)) {
-		if (!cpumask_test_cpu(cpu, p->cpus_ptr))
+		if (cpu == target || !cpumask_test_cpu(cpu, p->cpus_ptr))
 			continue;
 		if (available_idle_cpu(cpu) || sched_idle_cpu(cpu))
 			return cpu;
