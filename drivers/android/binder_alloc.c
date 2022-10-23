dda45ac5689e175c7de45e67b152efaf68320c32
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-10-23 11:05:08 +0530

    kernel: Implement xiaomi MIUI user experience optimizations
    From: micode/Xiaomi_Kernel_Opensource
    branch: munch-s-oss
    
    Change-Id: I66bc618b308766cee00fc5acf70ca826f205855a
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -451,6 +451,7 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 		binder_alloc_debug(BINDER_DEBUG_BUFFER_ALLOC, "%s", NAME_ARRAY[0]);
 	}
 #endif
+
 	if (is_async &&
 	    alloc->free_async_space < size + sizeof(struct binder_buffer)) {
 		binder_alloc_debug(BINDER_DEBUG_BUFFER_ALLOC,
