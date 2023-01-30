8f1ae5762e15ec4654c48cc19cd0f421367a21c3
Danda420 <dandagaharu77@gmail.com>
2023-01-30 13:53:18 +0700

    silence warning

--- drivers/input/fingerprint/goodix/gf_spi.c
+++ drivers/input/fingerprint/goodix/gf_spi.c
@@ -569,14 +569,12 @@ static irqreturn_t gf_irq(int irq, void *handle)
 	pr_debug("%s enter\n", __func__);
 	__pm_wakeup_event(fp_wakelock, WAKELOCK_HOLD_TIME);
 	sendnlmsg(temp);
-
-#if IS_ENABLED(CONFIG_MI_DRM_OPT)
+	
 	if ((gf_dev->wait_finger_down == true) && (gf_dev->device_available == 1) &&
 		(gf_dev->fb_black == 1)) {
 		gf_dev->wait_finger_down = false;
 		schedule_work(&gf_dev->work);
 	}
-#endif
 
 
 #elif defined (GF_FASYNC)
