ff8216f26c3c2adab2bd2e0a2cff41f2720c38eb
EmanuelCN <emanuelghub@gmail.com>
2023-06-20 17:28:09 +0000

    Revert "binder: extend kmem cache to binder_task_work_cb and binder_txn_fd_fixup"
    This reverts commit 8e167ae52fe9f56f8dd7076a6f7e6e9e18d8666d.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -159,8 +159,6 @@ static struct kmem_cache *binder_ref_pool;
 static struct kmem_cache *binder_thread_pool;
 static struct kmem_cache *binder_transaction_pool;
 static struct kmem_cache *binder_work_pool;
-static struct kmem_cache *binder_twcb_pool;
-static struct kmem_cache *binder_fixup_pool;
 
 static struct binder_stats binder_stats;
 static inline void binder_stats_deleted(enum binder_stat_types type)
@@ -1543,7 +1541,7 @@ static void binder_free_txn_fixups(struct binder_transaction *t)
 	list_for_each_entry_safe(fixup, tmp, &t->fd_fixups, fixup_entry) {
 		fput(fixup->file);
 		list_del(&fixup->fixup_entry);
-		kmem_cache_free(binder_fixup_pool, fixup);
+		kfree(fixup);
 	}
 }
 static void binder_txn_latency_free(struct binder_transaction *t)
@@ -1864,7 +1862,7 @@ static void binder_do_fd_close(struct callback_head *twork)
 	struct binder_task_work_cb *twcb = container_of(twork,
 			struct binder_task_work_cb, twork);
 	fput(twcb->file);
-	kmem_cache_free(binder_twcb_pool, twcb);
+	kfree(twcb);
 }
 /**
  * binder_deferred_fd_close() - schedule a close for the given file-descriptor
@@ -1876,7 +1874,7 @@ static void binder_do_fd_close(struct callback_head *twork)
 static void binder_deferred_fd_close(int fd)
 {
 	struct binder_task_work_cb *twcb;
-	twcb = kmem_cache_alloc(binder_twcb_pool, GFP_KERNEL);
+	twcb = kmalloc(sizeof(*twcb), GFP_KERNEL);
 	if (!twcb)
 		return;
 	init_task_work(&twcb->twork, binder_do_fd_close);
@@ -1885,7 +1883,7 @@ static void binder_deferred_fd_close(int fd)
 		filp_close(twcb->file, current->files);
 		task_work_add(current, &twcb->twork, true);
 	} else {
-		kmem_cache_free(binder_twcb_pool, twcb);
+		kfree(twcb);
 	}
 }
 static void binder_transaction_buffer_release(struct binder_proc *proc,
@@ -2215,7 +2213,7 @@ static int binder_translate_fd(u32 fd, binder_size_t fd_offset,
 	 * of the fd in the target needs to be done from a
 	 * target thread.
 	 */
-	fixup = kmem_cache_alloc(binder_fixup_pool, GFP_KERNEL);
+	fixup = kmalloc(sizeof(*fixup), GFP_KERNEL);
 	if (!fixup) {
 		ret = -ENOMEM;
 		goto err_alloc;
@@ -3838,7 +3836,7 @@ static int binder_apply_fd_fixups(struct binder_proc *proc,
 				binder_deferred_fd_close(fd);
 		}
 		list_del(&fixup->fixup_entry);
-		kmem_cache_free(binder_fixup_pool, fixup);
+		kfree(fixup);
 	}
 	return ret;
 }
@@ -5754,20 +5752,8 @@ static int __init binder_create_pools(void)
 	if (!binder_work_pool)
 		goto err_work_pool;
 
-	binder_twcb_pool = KMEM_CACHE(binder_task_work_cb, SLAB_HWCACHE_ALIGN);
-	if (!binder_twcb_pool)
-		goto err_twcb_pool;
-
-	binder_fixup_pool = KMEM_CACHE(binder_txn_fd_fixup, SLAB_HWCACHE_ALIGN);
-	if (!binder_fixup_pool)
-		goto err_fixup_pool;
-
 	return 0;
 
-err_fixup_pool:
-	kmem_cache_destroy(binder_twcb_pool);
-err_twcb_pool:
-	kmem_cache_destroy(binder_work_pool);
 err_work_pool:
 	kmem_cache_destroy(binder_transaction_pool);
 err_transaction_pool:
@@ -5795,8 +5781,6 @@ static void __init binder_destroy_pools(void)
 	kmem_cache_destroy(binder_thread_pool);
 	kmem_cache_destroy(binder_transaction_pool);
 	kmem_cache_destroy(binder_work_pool);
-	kmem_cache_destroy(binder_twcb_pool);
-	kmem_cache_destroy(binder_fixup_pool);
 }
 
 static int __init binder_init(void)
