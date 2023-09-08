577f8d0b9a9dd021132c1a2590f12f88e146f764
Liao Chang <liaochang1@huawei.com>
2023-09-08 03:16:04 +0000

    cpufreq: schedutil: Merge initialization code of sg_cpu in single loop
    The initialization code of the per-cpu sg_cpu struct is currently split
    into two for-loop blocks. This can be simplified by merging the two
    blocks into a single loop. This will make the code more maintainable.
    
    Signed-off-by: Liao Chang <liaochang1@huawei.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -845,11 +845,6 @@ static int sugov_start(struct cpufreq_policy *policy)
 		memset(sg_cpu, 0, sizeof(*sg_cpu));
 		sg_cpu->cpu			= cpu;
 		sg_cpu->sg_policy		= sg_policy;
-	}
-
-	for_each_cpu(cpu, policy->cpus) {
-		struct sugov_cpu *sg_cpu = &per_cpu(sugov_cpu, cpu);
-
 		cpufreq_add_update_util_hook(cpu, &sg_cpu->update_util,
 					     policy_is_shared(policy) ?
 							sugov_update_shared :
