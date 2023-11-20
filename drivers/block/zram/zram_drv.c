bd7563d002e05a263514efbfafd2cd7174beaa97
Danda420 <dandagaharu77@gmail.com>
2023-11-20 22:16:28 +0700

    Revert "zram_drv: Hardcode disksize to 4GB"
    This reverts commit 4452c1d1709b9f67327297e5b39db026edcc788d.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1790,7 +1790,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = (u64)4096 * SZ_1M;
+	disksize = memparse(buf, NULL);
 	if (!disksize)
 		return -EINVAL;
 
