c43dfecde14a959c6e6506b46c00ebb32bb591de
Rudi Setiyawan <diphons@gmail.com>
2023-12-10 21:58:23 +0700

    drivers/gpu/drm: drm_atomic: use new name of ddrbw boost Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2608,7 +2608,7 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 		return -EINVAL;
 
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
 
