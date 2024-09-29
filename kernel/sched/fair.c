ce7d02ec1a64ec8248fe63ce59e92a648434077b
Danda420 <dandagaharu77@gmail.com>
2024-09-29 20:49:21 +0700

    sched: guard WALT i forgot to push this lmao

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6404,7 +6404,6 @@ long schedtune_task_margin(struct task_struct *task)
 	return margin;
 }
 
-#ifdef CONFIG_SCHED_WALT
 unsigned long
 stune_util(int cpu, unsigned long other_util,
 		 struct sched_walt_cpu_load *walt_load)
@@ -6417,7 +6416,6 @@ stune_util(int cpu, unsigned long other_util,
 
 	return util + margin;
 }
-#endif
 
 #else /* CONFIG_SCHED_TUNE */
 
