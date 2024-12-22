9fa118ed8addedc05f78be2484c107af62e9b9e6
Danda420 <dandagaharu77@gmail.com>
2024-12-22 23:50:37 +0700

    Revert "Add support KernelSU Magic"
    This reverts commit 6d4ad7740183443fed3674e802d9222e8f0ff42c.

--- drivers/kernelsu/apk_sign.c
+++ drivers/kernelsu/apk_sign.c
@@ -330,8 +330,5 @@ module_param_cb(ksu_debug_manager_uid, &expected_size_ops,
 
 bool ksu_is_manager_apk(char *path)
 {
-	return (check_v2_signature(path, 0x363, "4359c171f32543394cbc23ef908c4bb94cad7c8087002ba164c8230948c21549") // dummy.keystore
-	|| check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH)  // ksu official
-/*	|| check_v2_signature(path, custom_size, custom_hash)  // add more as you like 	*/
-	);
+	return check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH);
 }
