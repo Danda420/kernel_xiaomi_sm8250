3191d6ac72c5239a3bb3d3b98463c20d2dfaf737
Dietmar Eggemann <dietmar.eggemann@arm.com>
2025-01-16 09:31:51 +0000

    sched/deadline: Remove unused def_dl_bandwidth
    Since commit 1724813 ("sched/deadline: Remove the sysctl_sched_dl
    knobs") the default deadline bandwidth control structure has no purpose.
    Remove it.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Juri Lelli <juri.lelli@redhat.com>
    Link: https://lore.kernel.org/r/20220302183433.333029-2-dietmar.eggemann@arm.com
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -19,8 +19,6 @@
 #include "pelt.h"
 #include "walt.h"
 
-struct dl_bandwidth def_dl_bandwidth;
-
 static inline struct task_struct *dl_task_of(struct sched_dl_entity *dl_se)
 {
 	return container_of(dl_se, struct task_struct, dl);
@@ -384,12 +382,10 @@ void init_dl_bandwidth(struct dl_bandwidth *dl_b, u64 period, u64 runtime)
 void init_dl_bw(struct dl_bw *dl_b)
 {
 	raw_spin_lock_init(&dl_b->lock);
-	raw_spin_lock(&def_dl_bandwidth.dl_runtime_lock);
 	if (global_rt_runtime() == RUNTIME_INF)
 		dl_b->bw = -1;
 	else
 		dl_b->bw = to_ratio(global_rt_period(), global_rt_runtime());
-	raw_spin_unlock(&def_dl_bandwidth.dl_runtime_lock);
 	dl_b->total_bw = 0;
 }
 
@@ -2602,9 +2598,6 @@ void sched_dl_do_global(void)
 	struct dl_bw *dl_b;
 	int cpu;
 	unsigned long flags;
-
-	def_dl_bandwidth.dl_period = global_rt_period();
-	def_dl_bandwidth.dl_runtime = global_rt_runtime();
 
 	if (global_rt_runtime() != RUNTIME_INF)
 		new_bw = to_ratio(global_rt_period(), global_rt_runtime());
