276b487d73fe392261866c65ad143fc304b2001b
David Oberhollenzer <david.oberhollenzer@sigma-star.at>
2025-04-01 15:56:37 +0200

    net: dsa: mv88e6xxx: propperly shutdown PPU re-enable timer on destroy
    [ Upstream commit a58d882841a0750da3c482cd3d82432b1c7edb77 ]
    
    The mv88e6xxx has an internal PPU that polls PHY state. If we want to
    access the internal PHYs, we need to disable the PPU first. Because
    that is a slow operation, a 10ms timer is used to re-enable it,
    canceled with every access, so bulk operations effectively only
    disable it once and re-enable it some 10ms after the last access.
    
    If a PHY is accessed and then the mv88e6xxx module is removed before
    the 10ms are up, the PPU re-enable ends up accessing a dangling pointer.
    
    This especially affects probing during bootup. The MDIO bus and PHY
    registration may succeed, but registration with the DSA framework
    may fail later on (e.g. because the CPU port depends on another,
    very slow device that isn't done probing yet, returning -EPROBE_DEFER).
    In this case, probe() fails, but the MDIO subsystem may already have
    accessed the MIDO bus or PHYs, arming the timer.
    
    This is fixed as follows:
     - If probe fails after mv88e6xxx_phy_init(), make sure we also call
       mv88e6xxx_phy_destroy() before returning
     - In mv88e6xxx_remove(), make sure we do the teardown in the correct
       order, calling mv88e6xxx_phy_destroy() after unregistering the
       switch device.
     - In mv88e6xxx_phy_destroy(), destroy both the timer and the work item
       that the timer might schedule, synchronously waiting in case one of
       the callbacks already fired and destroying the timer first, before
       waiting for the work item.
     - Access to the PPU is guarded by a mutex, the worker acquires it
       with a mutex_trylock(), not proceeding with the expensive shutdown
       if that fails. We grab the mutex in mv88e6xxx_phy_destroy() to make
       sure the slow PPU shutdown is already done or won't even enter, when
       we wait for the work item.
    
    Fixes: 2e5f032095ff ("dsa: add support for the Marvell 88E6131 switch chip")
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@sigma-star.at>
    Reviewed-by: Vladimir Oltean <olteanv@gmail.com>
    Link: https://patch.msgid.link/20250401135705.92760-1-david.oberhollenzer@sigma-star.at
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

--- drivers/net/dsa/mv88e6xxx/chip.c
+++ drivers/net/dsa/mv88e6xxx/chip.c
@@ -4886,12 +4886,12 @@ static int mv88e6xxx_probe(struct mdio_device *mdiodev)
 	err = mv88e6xxx_switch_reset(chip);
 	mutex_unlock(&chip->reg_lock);
 	if (err)
-		goto out;
+		goto out_phy;
 
 	chip->irq = of_irq_get(np, 0);
 	if (chip->irq == -EPROBE_DEFER) {
 		err = chip->irq;
-		goto out;
+		goto out_phy;
 	}
 
 	/* Has to be performed before the MDIO bus is created, because
@@ -4906,7 +4906,7 @@ static int mv88e6xxx_probe(struct mdio_device *mdiodev)
 	mutex_unlock(&chip->reg_lock);
 
 	if (err)
-		goto out;
+		goto out_phy;
 
 	if (chip->info->g2_irqs > 0) {
 		err = mv88e6xxx_g2_irq_setup(chip);
@@ -4946,6 +4946,8 @@ out_g1_irq:
 		mv88e6xxx_g1_irq_free(chip);
 	else
 		mv88e6xxx_irq_poll_free(chip);
+out_phy:
+	mv88e6xxx_phy_destroy(chip);
 out:
 	if (pdata)
 		dev_put(pdata->netdev);
@@ -4963,7 +4965,6 @@ static void mv88e6xxx_remove(struct mdio_device *mdiodev)
 		mv88e6xxx_ptp_free(chip);
 	}
 
-	mv88e6xxx_phy_destroy(chip);
 	mv88e6xxx_unregister_switch(chip);
 	mv88e6xxx_mdios_unregister(chip);
 
@@ -4977,6 +4978,8 @@ static void mv88e6xxx_remove(struct mdio_device *mdiodev)
 		mv88e6xxx_g1_irq_free(chip);
 	else
 		mv88e6xxx_irq_poll_free(chip);
+
+	mv88e6xxx_phy_destroy(chip);
 }
 
 static const struct of_device_id mv88e6xxx_of_match[] = {
