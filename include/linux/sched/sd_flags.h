ce642d32227b9bd3bcc153d1bb00ca7d446f16ec
Rudi Setiyawan <diphons@gmail.com>
2025-05-08 08:49:37 +0700

    sched/topology: Mark SD_LOAD_BALANCE as SDF_NEEDS_GROUPS Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- include/linux/sched/sd_flags.h
+++ include/linux/sched/sd_flags.h
@@ -46,8 +46,9 @@
  * Do load balancing on this domain
  *
  * SHARED_CHILD: Set from the base domain up to cpuset.sched_relax_domain_level.
+ * NEEDS_GROUPS: Load balancing flag.
  */
-SD_FLAG(SD_LOAD_BALANCE, SDF_SHARED_CHILD)
+SD_FLAG(SD_LOAD_BALANCE, SDF_SHARED_CHILD | SDF_NEEDS_GROUPS)
 
 /*
  * Balance when about to become idle
