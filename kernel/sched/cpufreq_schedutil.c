4ad009ea6b0504bca0c1944ea959577f5f2d5220
EmanuelCN <emanuelghub@gmail.com>
2025-06-01 21:39:55 +0300

    Revert "sched: Introduce Per-Cluster DVFS Headroom"
    This reverts commit a89691f41bcd3fb3e2113da44973571e3a83fc2e.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -252,25 +252,12 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	return l_freq;
 }
 
-static __always_inline
-unsigned long apply_dvfs_headroom(int cpu, unsigned long util)
-{
-	unsigned long headroom;
-
-	if (cpumask_test_cpu(cpu, cpu_lp_mask))
-		headroom = util + (util >> 1);
-	else
-		headroom = util + (util >> 2);
-
-	return headroom;
-}
-
 unsigned long sugov_effective_cpu_perf(int cpu, unsigned long actual,
 				 unsigned long min,
 				 unsigned long max)
 {
 	/* Add dvfs headroom to actual utilization */
-	actual = apply_dvfs_headroom(cpu, actual);
+	actual = map_util_perf(actual);
 	/* Actually we don't need to target the max performance */
 	if (actual < max)
 		max = actual;
