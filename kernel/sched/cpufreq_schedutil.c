66fab9cfbdbfc1e42b95c37a26468ea070d506b7
John Galt <johngaltfirstrun@gmail.com>
2025-06-13 14:25:12 -0400

    cpufreq: schedutil: Apply quadratic tapering to dvfs headroom
    Should better match real world power performance curve.
    
    Precompute factor to have less of a performance cost given added
    complexity.
    
    Note: This could have also been done with sched_dvfs_headroom precomputed
    earlier.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -254,24 +254,21 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 
 static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
 {
-        unsigned int sched_dvfs_headroom[8] = { [0 ... 7] = 1280 };
-        unsigned long capacity = capacity_orig_of(cpu);
-        unsigned long headroom;
+	unsigned long capacity = capacity_orig_of(cpu);
+	unsigned long delta;
+	unsigned long headroom;
 
-        if (util >= capacity)
-                return util;
+	if (util >= capacity)
+		return util;
 
-        /*
-         * Taper the boosting at e top end as these are expensive and
-         * we don't need that much of a big headroom as we approach max
-         * capacity
-         *
-         */
-        headroom = (capacity - util);
-        /* formula: headroom * (1.X - 1) == headroom * 0.X */
-        headroom = headroom *
-                (sched_dvfs_headroom[cpu] - SCHED_CAPACITY_SCALE) >> SCHED_CAPACITY_SHIFT;
-        return util + headroom;
+	/*
+	 * Quadratic taper the boosting at the top end as these are expensive and
+	 * we don't need that much of a big headroom as we approach max capacity
+	 */
+	delta = capacity - util;
+	headroom = (delta * delta * factor[cpu]) >> (SCHED_CAPACITY_SHIFT + 10);
+
+	return util + headroom;
 }
 
 unsigned long sugov_effective_cpu_perf(int cpu, unsigned long actual,
