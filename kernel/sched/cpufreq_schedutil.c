6fdfac226fe0ef31604dff30e1fb2c4b07c9c60a
EmanuelCN <emanuelghub@gmail.com>
2025-06-21 17:56:18 +0300

    cpufreq: schedutil: Make DVFS headroom capacity-aware and add low-util suppression
    Modify DVFS headroom calculation to be capacity-aware:
    
    1. Replace fixed shift headroom with (delta^2)/(4 * capacity)
       - Scales with CPU capacity
       - Maintains quadratic tapering at high utilization
    
    2. Add 10% capacity threshold for low-util suppression
       - Quadratically scales headroom below threshold
       - Prevents unnecessary boosting for very light workloads
    
    These changes optimize headroom for big.LITTLE systems by:
    - Adapting headroom calculation to CPU capacity
    - Eliminating useless boosts during low utilization
    - Maintaining performance while improving power consumption
    
    Signed-off-by: Danda420 <dandagaharu77@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -254,21 +254,28 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 
 static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
 {
-	unsigned long capacity = capacity_orig_of(cpu);
-	unsigned long delta, headroom;
+    	unsigned long capacity = capacity_orig_of(cpu);
+    	unsigned long delta, headroom, min_util;
 
-	if (util >= capacity)
-		return util;
-
-	/*
-	 * Quadratic taper the boosting at the top end as these are expensive
-	 * and we don't need that much of a big headroom as we approach max
-	 * capacity
-	 */
+    	if (util >= capacity)
+        	return util;
+        /*
+         * Quadratic taper the boosting at the top end as these are expensive
+         * and we don't need that much of a big headroom as we approach max
+         * capacity
+         */
 	delta = capacity - util;
-	headroom = ((delta * delta) >> 12);
+	headroom = (delta * delta) / (4 * capacity);
 
-	return util + headroom;
+	/* 10% of capacity threshold */
+    	min_util = capacity / 10;
+
+    	/* Suppress boosting below the threshold */
+    	if (util < min_util) {
+        	headroom = (headroom * util * util) / (min_util * min_util);
+    	}
+
+    	return util + headroom;
 }
 
 unsigned long sugov_effective_cpu_perf(int cpu, unsigned long actual,
