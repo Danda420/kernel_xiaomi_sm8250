a4e4fb0168b787b875a369e10dbf02cd2bc709d6
Danda420 <dandagaharu77@gmail.com>
2025-07-04 14:06:21 +0700

    drivers: kernelsu: add SukiSU support

--- drivers/kernelsu/apk_sign.c
+++ drivers/kernelsu/apk_sign.c
@@ -334,5 +334,6 @@ bool ksu_is_manager_apk(char *path)
 	|| check_v2_signature(path, EXPECTED_LEGACY_SIZE, EXPECTED_LEGACY_HASH) 
 	|| check_v2_signature(path, EXPECTED_NEXT_SIZE, EXPECTED_NEXT_HASH) 
 	|| check_v2_signature(path, EXPECTED_N3X7G3N_SIZE, EXPECTED_N3X7G3N_HASH) 
-	|| check_v2_signature(path, EXPECTED_DKM_SIZE, EXPECTED_DKM_HASH));
+	|| check_v2_signature(path, EXPECTED_DKM_SIZE, EXPECTED_DKM_HASH)
+	|| check_v2_signature(path, EXPECTED_SUKISU_SIZE, EXPECTED_SUKISU_HASH));
 }
