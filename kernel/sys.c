02cc8a7a2a9ebc79a7093311cc3b17dd5e9a7a74
Danda420 <dandagaharu77@gmail.com>
2026-01-20 23:45:55 +0700

    syscall: fake uname to 5.10.248 for bpfloader/netbpfload/netd

--- kernel/sys.c
+++ kernel/sys.c
@@ -1249,6 +1249,14 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+	if (current_uid().val == 0 &&
+		(!strncmp(current->comm, "bpfloader", 9) ||
+		!strncmp(current->comm, "netbpfload", 10) ||
+		!strncmp(current->comm, "netd", 4))) {
+		strcpy(tmp.release, "5.10.248");
+		pr_info("fake uname: %s/%d release=%s\n",
+			 current->comm, current->pid, tmp.release);
+	}
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
 #endif
